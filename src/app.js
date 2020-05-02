c90cab1c60cac788e18f6cc29ef16479e7c27a77
ckerpel <ckerpel@users.noreply.github.com>
2020-05-02 20:49:07 +0100

    Color Adjustments

--- src/app.js
+++ src/app.js
@@ -129,27 +129,26 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  
-  iconDayNight = response.data.weather[0].icon;
-  iconSetting = iconDayNight.slice(2);
-  console.log(iconSetting);
-
-  backgroundSetting = 'BBB';
-  console.log(backgroundSetting);
-
-  function whatColor(response){
-    return response.name;
-  }
-  
-  let settings = [
-      {Name: 'AAA' , color:'teal'},
-      {Name: 'BBB' , color:'green'},
-      {Name: 'CCC' , color:'blue'},
-      {Name: 'DDD' , color:'pink'}
-    ];
     
-    console.log(settings.find(whatColor));
+  backgroungColor.call();
   
+    function backgroungColor(){
+    iconDayNight = response.data.weather[0].icon;
+    iconSetting = iconDayNight.slice(2);  
+    backgroundSetting = `${iconSetting}${response.data.weather[0].description}`;
+    if(iconSetting = "d")
+      {document.body.style.backgroundColor = "linear-gradient( 109.6deg,  rgba(0,0,0,1) 11.2%, rgba(11,132,145,1) 91.1% )"}
+      else if(iconSetting = "n")
+      {document.body.style.backgroundColor = "linear-gradient( 109.6deg,  rgba(0,0,0,1) 11.2%, rgba(11,132,145,1) 91.1% )"}
+      else (backgroundSetting === null)
+      {document.body.style.backgroundColor = "linear-gradient( 109.6deg,  rgba(0,0,0,1) 11.2%, rgba(11,132,145,1) 91.1% )"}
+    
+    console.log(backgroundSetting);
+    console.log(iconDayNight);
+    console.log(iconSetting );
+  }
+
+
     
    //Bring Date info based on the browser
   //nowDayElement.innerHTML = formatDate(response.data.dt * 1000) + " ,"+ formatHours(response.data.dt * 1000);
